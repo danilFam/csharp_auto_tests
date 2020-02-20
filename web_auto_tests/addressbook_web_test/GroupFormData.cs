662e5330cf8dd297b956391c2cd80d522f17e7af
danilFam <danil.bondar@gmail.com>
2020-02-21 01:59:14 +0200

    methods and class for task3

---
+++ web_auto_tests/addressbook_web_test/GroupFormData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_web_test
+{
+    class GroupFormData
+    {
+        string name;
+        string header="";
+        string footer="";
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Header
+        {
+            get { return header;}
+            set { header = value; }
+        }
+        public string Footer
+        {
+            get { return footer; }
+            set { footer = value; }
+        }
+       
+        public GroupFormData(string name)
+        {
+            this.name = name;
+        }
+        
+    }
+}
--- web_auto_tests/addressbook_web_test/NewGroupTest.cs
+++ web_auto_tests/addressbook_web_test/NewGroupTest.cs
@@ -11,6 +11,9 @@ using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Interactions;
 using NUnit.Framework;
+
+namespace addressbook_web_test
+{ 
 [TestFixture]
 public class NewgroupTest
 {
@@ -30,23 +33,66 @@ public class NewgroupTest
         driver.Quit();
     }
     [Test]
-    public void newgroup()
+    public void Newgroup()
     {
-        driver.Navigate().GoToUrl("http://localhost/addressbook/");
-        driver.Manage().Window.Size = new System.Drawing.Size(758, 984);
-        driver.FindElement(By.Name("user")).SendKeys("admin");
-        driver.FindElement(By.Name("pass")).SendKeys("secret");
-        driver.FindElement(By.CssSelector("input:nth-child(7)")).Click();
-        driver.FindElement(By.LinkText("groups")).Click();
-        driver.FindElement(By.Name("new")).Click();
-        driver.FindElement(By.Name("group_name")).Click();
-        driver.FindElement(By.Name("group_name")).SendKeys("new");
-        driver.FindElement(By.Name("group_header")).Click();
-        driver.FindElement(By.Name("group_header")).SendKeys("best");
-        driver.FindElement(By.Name("group_footer")).Click();
-        driver.FindElement(By.Name("group_footer")).SendKeys("group");
-        driver.FindElement(By.Name("submit")).Click();
-        driver.FindElement(By.LinkText("group page")).Click();
+        OpenHomePage();
+        Login("admin", "secret");
+        GoToGroupsPage();
+        InitGroupCreation();
+        GroupFormData group = new GroupFormData("new");
+            group.Footer = "123";
+        FillGroupForm(group);
+        SubmitGroupCreation();
+        ReturnToGroupsPage();
+        Logout();
+    }
+        
+    private void Logout()
+    {
         driver.FindElement(By.LinkText("Logout")).Click();
     }
+
+    private void ReturnToGroupsPage()
+    {
+        driver.FindElement(By.LinkText("group page")).Click();
+    }
+
+    private void SubmitGroupCreation()
+    {
+        driver.FindElement(By.Name("submit")).Click();
+    }
+
+    private void FillGroupForm(GroupFormData group)
+    {
+        driver.FindElement(By.Name("group_name")).Click();
+        driver.FindElement(By.Name("group_name")).SendKeys(group.Name);
+        driver.FindElement(By.Name("group_header")).Click();
+        driver.FindElement(By.Name("group_header")).SendKeys(group.Header);
+        driver.FindElement(By.Name("group_footer")).Click();
+        driver.FindElement(By.Name("group_footer")).SendKeys(group.Footer);
+    }
+
+    private void InitGroupCreation()
+    {
+        driver.FindElement(By.Name("new")).Click();
+    }
+
+    private void GoToGroupsPage()
+    {
+        driver.FindElement(By.LinkText("groups")).Click();
+    }
+
+    private void Login(string username, string password)
+    {
+        driver.Manage().Window.Size = new System.Drawing.Size(758, 984);
+        driver.FindElement(By.Name("user")).SendKeys(username);
+        driver.FindElement(By.Name("pass")).SendKeys(password);
+        driver.FindElement(By.CssSelector("input:nth-child(7)")).Click();
+    }
+
+    private void OpenHomePage()
+    {
+        driver.Navigate().GoToUrl("http://localhost/addressbook/");
+    }
+}
 }
