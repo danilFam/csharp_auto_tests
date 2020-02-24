aef713a961036fa4549c2685ee7e6abae2dec310
danilFam <danil.bondar@gmail.com>
2020-02-25 00:09:51 +0200

    привел код в порядок

--- web_auto_tests/addressbook_web_test/ContactFormData.cs
+++ web_auto_tests/addressbook_web_test/ContactFormData.cs
@@ -1,113 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace addressbook_web_test
+﻿namespace addressbook_web_test
 {
     class ContactFormData
     {
-        string firstname, middlename = "", lastname = "", nickname = "",
-            title = "", company = "", address = "", home = "",
-            mobile = "", work = "", fax = "", email = "",
-            email2 = "", email3 = "", homepage = "", address2 = "",
-            phone2 = "", notes = "";
-
-        public string Firstname
-        {
-            get { return firstname; }
-            set { firstname = value; }
-        }
-        public string Middlename
-        {
-            get { return middlename; }
-            set { middlename = value; }
-        }
-        public string Lastname
-        {
-            get { return lastname; }
-            set { lastname = value; }
-        }
-        public string Nickname
-        {
-            get { return nickname; }
-            set { nickname = value; }
-        }
-        public string Title
-        {
-            get { return title; }
-            set { title = value; }
-        }
-        public string Company
-        {
-            get { return company; }
-            set { company = value; }
-        }
-        public string Address
-        {
-            get { return address; }
-            set { address = value; }
-        }
-        public string Home
-        {
-            get { return home; }
-            set { home = value; }
-        }
-        public string Mobile
-        {
-            get { return mobile; }
-            set { mobile = value; }
-        }
-        public string Work
-        {
-            get { return work; }
-            set { work = value; }
-        }
-        public string Fax
-        {
-            get { return fax; }
-            set { fax = value; }
-        }
-        public string Email
-        {
-            get { return email; }
-            set { email = value; }
-        }
-        public string Email2
-        {
-            get { return email2; }
-            set { email2 = value; }
-        }
-        public string Email3
-        {
-            get { return email3; }
-            set { email3 = value; }
-        }
-        public string Homepage
-        {
-            get { return homepage; }
-            set { homepage = value; }
-        }
-        public string Address2
-        {
-            get { return address2; }
-            set { address2 = value; }
-        }
-        public string Phone2
-        {
-            get { return phone2; }
-            set { phone2 = value; }
-        }
-        public string Notes
-        {
-            get { return notes; }
-            set { notes = value; }
-        }
         public ContactFormData(string firstname)
         {
-            this.firstname = firstname;
+            this.Firstname = firstname;
         }
+
+        public string Firstname { get; set; }
+        public string Middlename { get; set; } = "";
+        public string Lastname { get; set; } = "";
+        public string Nickname { get; set; } = "";
+        public string Title { get; set; } = "";
+        public string Company { get; set; } = "";
+        public string Address { get; set; } = "";
+        public string Home { get; set; } = "";
+        public string Mobile { get; set; } = "";
+        public string Work { get; set; } = "";
+        public string Fax { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Email2 { get; set; } = "";
+        public string Email3 { get; set; } = "";
+        public string Homepage { get; set; } = "";
+        public string Address2 { get; set; } = "";
+        public string Phone2 { get; set; } = "";
+        public string Notes { get; set; } = "";
+       
 
     }
 }
--- web_auto_tests/addressbook_web_test/GroupFormData.cs
+++ web_auto_tests/addressbook_web_test/GroupFormData.cs
@@ -1,36 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace addressbook_web_test
+﻿namespace addressbook_web_test
 {
     class GroupFormData
     {
-        string name;
-        string header="";
-        string footer="";
+        public string Name { get; set; }
+        public string Header { get; set; } = "";
+        public string Footer { get; set; } = "";
 
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-        public string Header
-        {
-            get { return header;}
-            set { header = value; }
-        }
-        public string Footer
-        {
-            get { return footer; }
-            set { footer = value; }
-        }
-       
         public GroupFormData(string name)
         {
-            this.name = name;
+            this.Name = name;
         }
         
     }
